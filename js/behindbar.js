88c45f3ed0160889034a51989755b276c3ed9183
hldzlk <zhoulink90@gmail.com>
2014-10-21 17:40:38 +0800

    0.1.4.1

--- js/behindbar.js
+++ js/behindbar.js
@@ -1,5 +1,6 @@
-;(function ($) { 
-/*使用方法
+;
+(function($) {
+	/*使用方法
 arr为所有要向右侧移动的块
 var arr=[".footerbar",".wtopbar",".mainlist",".paixu"];
 $("#categorybtn").controlbehindbar(arr);
@@ -17,36 +18,52 @@ html结构
 </div>
 </body>
 */
-$.fn.controlbehindbar = function (arr,listbox) {
-	$("body").wrapInner('<div class="sidebarbox"></div>');
-	$(".sidebarbox").append('<div class="sidebarhidebtn"></div>')
-	$(listbox).css({"min-height":$(window).height()});
-	$(".behindbar").show();
-	for(var i in arr){
-		$(arr[i]).addClass("transitionnomal")
-	}	
-	//测试侧栏
-	// for(var i in arr){
-	// 	$(arr[i]).addClass("sdiebartranslate")
-	// }
-	$(this).bind("touchend",function(e){
-		$(".sidebarhidebtn").show();
-		for(var i in arr){
-			$(arr[i]).addClass("sdiebartranslate")
+	$.fn.controlbehindbar = function(arr, listbox) {
+		$("body").wrapInner('<div class="sidebarbox"></div>');
+		$(".sidebarbox").append('<div class="sidebarhidebtn"></div>')
+		$(listbox).css({
+			"min-height": $(window).height()
+		});
+		$(".behindbar").show();
+		for (var i in arr) {
+			$(arr[i]).addClass("transitionnomal")
 		}
-	})
+		//测试侧栏
+		// for(var i in arr){
+		// 	$(arr[i]).addClass("sdiebartranslate")
+		// }
+		$(this).bind("touchend", function(e) {
+			$(".sidebarhidebtn").show();
+			for (var i in arr) {
+				$(arr[i]).addClass("sdiebartranslate")
+			}
+		})
 
-	$(".sidebarhidebtn").bind("touchstart",function(e){
-		e.preventDefault();
-		$(".sidebarhidebtn").hide();
-		for(var i in arr){
-			$(arr[i]).removeClass("sdiebartranslate")
-		}
-	})
-	return $(this);
-};
+		$(".sidebarhidebtn").bind("touchstart", function(e) {
+			e.preventDefault();
 
-})(jQuery); 
+			//复位动画禁点击
+			$(window).bind("click", function(e) {
+				e.preventDefault();
+			})
+			//动画完 取消禁点击
+			$(arr[0])[0].addEventListener("webkitTransitionEnd", cc)
 
+			function cc() {
+				$(arr[0])[0].removeEventListener("webkitTransitionEnd", cc)
+				setTimeout(function() {
+					$(window).unbind("click");
+				}, 500)
+			}
 
+			//复位动画
+			for (var i in arr) {
+				$(arr[i]).removeClass("sdiebartranslate")
+			}
+			$(".sidebarhidebtn").hide();
+		})
 
+		return $(this);
+	};
+
+})(jQuery);
