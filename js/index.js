be07df62696b807b2bba11758128561ce302a772
hldzlk <zhoulink90@gmail.com>
2014-10-22 11:11:33 +0800

    0.1.5

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
-var cx4 = $(".mainnav").find(".col-xs-4")
-cx4.css({
-	"height": cx4.width()
+mainnavheight()
+$(window).resize(function(){
+	mainnavheight();
+})
+
+function mainnavheight(){
+	var cx4 = $(".mainnav").find(".col-xs-4")
+	cx4.css({
+		"height": cx4.width()
+	})
+}
+
+
+$("#newslistbtn").click(function(){
+	if($("#newslistbtn").hasClass("wbtn_greenyellow"))return;
+	$("#discusslistbtn").removeClass("wbtn_yellow");
+	$("#newslistbtn").addClass("wbtn_greenyellow");
+	$(".discusslist").hide();
+	$(".articlelist").show();
+})
+$("#discusslistbtn").click(function(){
+	if($("#discusslistbtn").hasClass("wbtn_yellow"))return;
+	$("#newslistbtn").removeClass("wbtn_greenyellow");
+	$("#discusslistbtn").addClass("wbtn_yellow");
+	$(".articlelist").hide();
+	$(".discusslist").show();
 })
