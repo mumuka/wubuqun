c325c59686557771e64cb19ceccbdca9350082ec
hldzlk <zhoulink90@gmail.com>
2014-11-21 19:47:42 +0800

    导师列表点击问题

--- js/filterbox.js
+++ js/filterbox.js
@@ -1,9 +1,18 @@
 $("#filterbtn").bind("touchend",function(){
-	if(!$("#filterbox").attr("style")){
-		var filterboxheight=$(window).height()-136;
-		$("#filterbox").height(filterboxheight);
+	if($("#filterbox").hasClass("hidefilterbox")){
+		$("#filterbox").removeClass("hidefilterbox")
+		setTimeout(function(){
+			var filterboxheight=$(window).height()-136;
+			$("#filterbox").height(filterboxheight);
+		},100)
 	}else{
 		$("#filterbox").attr("style","")
+		function hidefilterbox(){
+			$("#filterbox").addClass("hidefilterbox")
+			$("#filterbox")[0].removeEventListener('webkitTransitionEnd', hidefilterbox, false);
+		}
+		$("#filterbox")[0].addEventListener('webkitTransitionEnd', hidefilterbox, false);
+
 	}
 })
 
