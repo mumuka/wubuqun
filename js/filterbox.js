b28df4f6246f22d7368efe91a2857f54c9fb3d7a
hldzlk <zhoulink90@gmail.com>
2014-11-22 19:17:06 +0800

    gx

--- js/filterbox.js
+++ js/filterbox.js
@@ -16,3 +16,34 @@ $("#filterbtn").bind("touchend",function(){
 	}
 })
 
+// var FilterBox=function(container){
+// 	this.c=$(container)
+// }
+// FilterBox.prototype.showbox = function() {
+// 	var _this=this;
+// 	_this.c.removeClass("hidefilterbox")
+// 	setTimeout(function(){
+// 		var filterboxheight=$(window).height()-136
+// 		_this.c.height(filterboxheight)
+// 	},100)
+// }
+// FilterBox.prototype.hidebox = function() {
+// 	var _this=this;
+// 	_this.c.attr("style","")
+// 	function hidefilterbox(){
+// 		_this.c.addClass("hidefilterbox")
+// 		_this.c[0].removeEventListener('webkitTransitionEnd', hidefilterbox, false)
+// 	}
+// 	_this.c[0].addEventListener('webkitTransitionEnd', hidefilterbox, false)
+// }
+
+
+// var filterbox=new FilterBox("#filterbox")
+// $("#filterbtn").bind("touchend",function(){
+// 	if($("#filterbox").hasClass("hidefilterbox")){
+// 		filterbox.showbox()
+// 	}else{
+// 		filterbox.hidebox()
+// 	}
+// })
+
