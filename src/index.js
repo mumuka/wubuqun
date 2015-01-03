172e5b84d0974bed40848c4066da9acf6d034039
mumuka <545264278@qq.com>
2015-01-03 10:09:47 +0800

    hrloveindex

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ define(["wubuslider"],function() {
 	})
 
 	function mainnavheight() {
-		var cx4 = $(".mainnav").find(".col-xs-4")
+		var cx4 = $(".mainnav>a")
 		cx4.css({
 			"height": cx4.width()
 		})
